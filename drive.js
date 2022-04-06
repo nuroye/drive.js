f82c79296c52bdd2a7495b8d7bee17f3daf151e8
Nurudeen Oyeleye <oyeleyenurudeen@yahoo.com>
2022-04-06 13:47:20 +0100

    New commit
    drive

---
+++ drive.js
@@ -0,0 +1,37 @@
+// First Question
+
+var age=0;
+checkDriverAge= prompt("What is your age?");
+
+if (Number(checkDriverAge) < 18) {
+	console.log("Sorry, you are too yound to drive this car. Powering off");
+} 
+else if (Number(checkDriverAge) > 18) {
+	console.log("Powering On. Enjoy the ride!");
+}
+else if(Number(checkDriverAge2) === 18){
+	console.log("Congratulations on your first year of driving. Enjoy the ride!");
+}
+function checkDriverAge(age){
+ 
+    return age;
+}
+
+// Second Question :
+
+var age=0;
+var age= function checkDriverAge2(age){
+    return age
+}
+
+checkDriverAge2= prompt("What is your age?");
+
+if (Number(checkDriverAge2) < 18) {
+	console.log("Sorry, you are too yound to drive this car. Powering off");
+} 
+else if (Number(checkDriverAge2) > 18) {
+	console.log("Powering On. Enjoy the ride!");
+}
+ else if(Number(checkDriverAge2) === 18){
+	console.log("Congratulations on your first year of driving. Enjoy the ride!");
+ }
